a896be407fade5bad55514a42ed0501935408759
wagner ramos <h@des666>
2019-11-14 18:17:04 -0300

    Planets14-11

--- src/components/menu.js
+++ src/components/menu.js
@@ -9,8 +9,8 @@ const Menu = () => {
     return(
         <div className="menu">
         <div className="container">
-        <nav className="navbar navbar-expand-md">
-        <a className="navbar-brand" href="">Infos Planet App</a>
+        <nav className="navbar navbar-expand-lg">
+        <a className="navbar-brand" href="./">Infos Planet App</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"> <span className="navbar-toggler-icon"></span> </button>
     <div className="collapse navbar-collapse" id="collapsibleNavbar">
         <ul className="navbar-nav">
